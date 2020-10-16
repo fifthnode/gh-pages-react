38646a3fef3daa9a5ff2796a9f012afe7bdf2c62
Randall Cox <rcoxwork@gmail.com>
2020-10-16 14:38:30 -0700

    Updates

---
+++ precache-manifest.7096dfc5c11f00c15bef2edcec636097.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae13701cf6e66e8ac39e80e48dfbcbd",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "af8fa0b341abf18d3c76",
+    "url": "/gh-pages-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ad78b6cbc104af3630c",
+    "url": "/gh-pages-react/static/js/2.fc5b355b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gh-pages-react/static/js/2.fc5b355b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8fa0b341abf18d3c76",
+    "url": "/gh-pages-react/static/js/main.e0349740.chunk.js"
+  },
+  {
+    "revision": "9cf7c9cd95c86e4913b2",
+    "url": "/gh-pages-react/static/js/runtime-main.60b3b0ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
